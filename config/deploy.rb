4feae3c7fa257c4a5662aeef8b25a5caba82b5dd
skswhwo <skswhwo@gmail.com>
2016-09-30 06:29:57 +0900

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ set :format, :airbrussh
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-append :linked_files, 'config/application.yml'
+append :linked_files, 'config/application.yml', 'config/*.pem'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
